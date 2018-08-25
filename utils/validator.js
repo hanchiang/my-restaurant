0fe96b20af6ac035efb6d269844a79532a497b37
Han Chiang <yaphc@hotmail.com>
2018-08-25 21:46:29 +0800

    Sanitise inputs when adding store

--- utils/validator.js
+++ utils/validator.js
@@ -1,8 +1,11 @@
-const { check, body } = require('express-validator/check');
+const { body } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter')
 
 module.exports.validateStore = [
   body('name').isString().exists({ checkFalsy: true }).withMessage('Please enter store name'),
+  sanitizeBody('name').escape(),
   body('description').isString().exists({ checkFalsy: true }).withMessage('Please enter store description'),
+  sanitizeBody('description').escape(),
   body('location.address').exists({ checkFalsy: true }).withMessage('Please enter store address'),
   body('location.coordinates').custom(([lat, lng], { req, location, path }) => {
     return (typeof lat === 'string' && typeof lng === 'string' && lat !== '' && lng !== '');
